fadd12614a71952901a7ca9e027e0be03ac662a5
Holger Graf-Reichrt <holger@s87.de>
2015-12-30 12:22:37 +0100

    Updates motoscout24 crawler

--- crawler/Crawler/Harley/Motoscout24.php
+++ crawler/Crawler/Harley/Motoscout24.php
@@ -16,58 +16,33 @@ class Crawler_Harley_Motoscout24
 	{
 		$items = array();
 		$browser = &new SimpleBrowser();
-		
-		$browser->get('http://www.motoscout24.de/Public/Search.aspx');
-		$browser->setField('framework1:Search:CtlMakeModel:ddlMake', '40');
-		$browser->setField('framework1:Search:DdAge:ddlAge','1');
-		$page = $browser->clickSubmitByName('framework1:Search:BtnSearch');
-		
-		if( !preg_match('/Suchergebnis/', $page ) )
+//http://www.autoscout24.de/Moto/ListGN.aspx?atype=B&mmvmk0=50040&mmvco=1&make=50040&pricefrom=500&cy=D&ustate=N%2CU&fromhome=1&results=80&desc=1&sort=age&page=1
+//http://ww4.autoscout24.de/?atype=B&make=50040&mmvmk0=50040&mmvco=1&fregto=1993&cy=D&powerscale=KW&zipc=D&ustate=N,U&sort=age&desc=1&results=20&page=1&event=sort&dtr=s
+
+		$url = 'http://ww4.autoscout24.de/?atype=B&make=50040&mmvmk0=50040&mmvco=1&fregto=1993&cy=D&powerscale=KW&zipc=D&ustate=N,U&sort=age&desc=1&results=20&page=1&event=sort&dtr=s';
+		//$page = $browser->get($url);
+		$page = $this->getTestPage();
+		if( !preg_match('/Suchergebnis/', $page) )
 		{
 				echo "Fehler bei ".get_class($this)."\n";
-echo $page;
 				return array();
 		}
-//$browser->click('reporting bugs');
-//$page = $browser->click('PHP 5 bugs only');
-/*preg_match('/status=Open.*?by=Any.*?(\d+)<\/a>/', $page, $matches);
-print $matches[1];*/
-//echo $page;
-//		$page = $this->getTestPage();
-		$dom = new DOMDocument();
-		if( !@$dom->loadHTML($page) )
-			exit("ERROR");
-		
-		//echo $page; 
-		$xpath = new DOMXPath($dom);
-		$todayDivs = $xpath->query("//*/span[@id='listOutput']/*/div[@class='list-item']");
-		foreach( $todayDivs as $todayDiv )
+		// extract the js articlesarry from page
+		preg_match('/.*var articlesFromServer = (.*)/', $page, $matches);
+		$jsArray = str_replace(' || [];', '',$matches[1]);
+		$result = json_decode($jsArray);
+		//echo $jsArray;
+		foreach( $result as $item )
 		{
-			$srcPath = $todayDiv->getNodePath();
-
-			$pricePath = $srcPath . "/*/div[@class='infoCar']/a";
-			if( ($priceItems = $xpath->query($pricePath) ) )
-			{
-				$url = 'http://www.motoscout24.de';
-				$url .= (string)$priceItems->item(0)->getAttribute('href');
-			}	
-			
-			$pricePath = $srcPath . "/*/div[@class='infoCar']/a/div[@id='clicableBar']/div/span[1]";
-			if( ($priceItems = $xpath->query($pricePath) ) )
-				$price = trim((string)$priceItems->item(0)->nodeValue);
-			
-			$titlePath = $srcPath . "/*/div[@class='infoCar']/*/div[@class='headcar']/a";
-			if( ($titleItems = $xpath->query($titlePath) ) )
-				$title = trim((string)$titleItems->item(0)->nodeValue);
-			
-			$detailsPath = $srcPath . "/*/div[@class='infoCar']/*/div[@class='txt-car']/span";
-			if( ($detailsItems = $xpath->query($detailsPath) ) )
-				$details = trim((string)$detailsItems->item(0)->nodeValue);
-
+			$title = $item->md.' Bj. '.$item->fr;
+			$price = $item->pp;
 			$time = mktime();
+			$itemId = $item->ei; // like 282282813
+			$url = 'http://ww3.autoscout24.de/classified/'.$itemId.'?asrc=st|as';
+			$details = $item->ct.', '.$item->zp;
 			$items[] = new Harley_ResultItem( $title, $url, $price, $time, $details );
 		}
 		return $items;
+
 	}
 }
-
